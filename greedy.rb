b9d14b50479f795d65ef8afb7dfd715a8394e252
Dan Weiner <daweiner16@gmail.com>
2016-04-17 17:11:38 -0400

    Add begin end loop to greedy

--- greedy.rb
+++ greedy.rb
@@ -4,34 +4,40 @@
 
 require 'active_support/inflector'
 
-puts "Please enter an amount (for instance 10.75, .75, etc.)"
-amount_entered = gets.to_f
-change_cents = amount_entered * 100
 coins = 0
 quarters = 0
 dimes = 0
 nickels = 0
 pennies = 0
 
+begin
+  puts "Please enter an amount (for instance 10.75, .75, etc.)"
+  amount_entered = gets.to_f
+  change_cents = amount_entered * 100
+  if ( amount_entered <= 0 )
+    puts "Amount entered invalid"
+  end
+end until ( amount_entered > 0 )
+
 
 while ( change_cents > 0 ) do
   
-  if (change_cents >= 25) 
+  if ( change_cents >= 25) 
     coins = coins += 1
     quarters = quarters += 1
     change_cents -= 25
 
-  elsif (change_cents >= 10) 
+  elsif ( change_cents >= 10 ) 
     coins = coins += 1
     dimes = dimes += 1
     change_cents -= 10
 
-  elsif (change_cents >= 5) 
+  elsif ( change_cents >= 5 ) 
     coins = coins += 1
     nickels = nickels += 1
     change_cents -= 5
   
-  else (change_cents >= 1) 
+  else ( change_cents >= 1 ) 
     coins = coins += 1
     pennies = pennies += 1
     change_cents -= 1
