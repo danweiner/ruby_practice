8ac7dd0bd16ce4a1ed0d4f4f7dfb86643860187e
Dan Weiner <daweiner16@gmail.com>
2016-04-25 10:34:14 -0400

    add tile methods

--- project10/tile.rb
+++ project10/tile.rb
@@ -25,6 +25,83 @@ class Tile
   end
 
   def draw
-    
+    if tile_is_drawn? && !hidden?
+      x1 = @column * WIDTH
+      y1 = @row * HEIGHT
+      x2 = x1 + WIDTH
+      y2 = y1
+      x3 = x2
+      y3 = y2 + HEIGHT
+      x4 = x1
+      y4 = y3
+      c = color
+      @@window.draw_quad(x1, y1, c, x2, y2, c, x3, y3, c, x4, y4, c, 2)
+      x_center = x1 + (WIDTH / 2)
+      x_text = x_center - @@font.text_width("#{@type}") / 2
+      y_text = y1 + 4
+      @@font.draw("#{@type}", x_text, y_text, 1)
+    end
+  end
+
+  def color
+    if is_player?
+      @@colors[:red]
+    elsif is_exit?
+      @@colors[:green]
+    elsif is_treasure?
+      @@colors[:gold]
+    else
+      @@colors[:blue]
+    end
+  end
+
+  def move_to(column, row)
+    @column = column
+    @row = row
+  end
+
+  def move_by(column_delta, row_delta)
+    move_to(@column + column_delta, @row + row_delta)
+  end
+
+  def is_treasure?
+    @type == TREASURE_TYPE
+  end
+
+  def is_start?
+    @type == START_TYPE
+  end
+
+  def is_exit?
+    @type == EXIT_TYPE
+  end
+
+  def is_player?
+    @type == PLAYER_TYPE
+  end
+
+  def is_empty?
+    @type == EMPTY_TYPE || @type == ' '    
+  end
+
+  def hidden?
+    @hidden
+  end
+
+  def hide!
+    @hidden = true
+  end
+
+  # Make a tile empty when a player picks up an object like a treasure
+  def make_empty
+    @type = EMPTY_TYPE
+  end
+
+  def tile_is_drawn?
+    !is_empty? && !is_start?
+  end
+
+  def tile_can_be_entered?
+    is_empty? || is_start? || is_treasure? || is_exit?
   end
 end
