41ad994bfbaeb77a3a83f759d1ac73768019e817
Dan Weiner <daweiner16@gmail.com>
2016-04-25 09:44:52 -0400

    add game methods

--- project10/amazing.rb
+++ project10/amazing.rb
@@ -2,11 +2,26 @@
 # To run the program, use ruby amazing.rb
 
 require 'gosu'
+require_relative 'game'
 
 class Amazing < Gosu::Window
+
   def initialize
     super(640, 640)
     self.caption = "Amazing"
+    @game = Game.new(self)
+  end
+
+  def update
+    @game.update
+  end
+
+  def draw
+    @game.draw
+  end
+
+  def button_down(id)
+    @game.button_down(id)
   end
 end
 
--- project10/game.rb
+++ project10/game.rb
@@ -3,7 +3,28 @@ require_relative 'level'
 require_relative 'player'
 
 class Game
-  LEVEL1 = []
+  LEVEL1 = [ 
+    "+——————————————————+", 
+    "|S.|...T...........|",
+    "|..|..--------.|++.|", 
+    "|..|.|....|T|..|T..|", 
+    "|.+|.|.E|.|....|...|", 
+    "|..|.|---.|-|..|...|", 
+    "|..|.|....|.|......|", 
+    "|+.|.|......|..|-|.|", 
+    "|..|.-----|.|..|+|.|", 
+    "|..|T.......|..|+|.|", 
+    "|.++--------+..|+|.|", 
+    "|.+....+++.....|+|.|", 
+    "|...++.....+++.|+|.|", 
+    "|---------------+..|", 
+    "|T+|......|.....|.||", 
+    "|..|...|.....+T.|.||", 
+    "|+....+|---------+.|", 
+    "|................+.|", 
+    "|T+|...++++++++++..|", 
+    "+——————————————————+"  
+  ]
 
   def initialize(window)
     @window     = window
@@ -14,14 +35,32 @@ class Game
   end
 
   def button_down(id)
-    
+    @level.button_down(id)
   end
 
   def update
-    
+    @level.update
+    if !@level.level_over?
+      @time_now = Time.now.to_i
+    end
   end
 
   def draw
-    
+    @level.draw
+    draw_hud
+  end
+
+  def draw_hud
+    if @level.level_over?
+      @font.draw("GAME OVER!", 170, 150, 10, 2, 2)
+      @font.draw("Collected #{@player.score} treasure in #{time_in_seconds} seconds!", 110, 300, 10)
+    else
+      @font.draw("Time: #{time_in_seconds}", 4, 2, 10)
+      @font.draw("Score: #{@player.score}", 510, 2, 10)
+    end
+  end
+
+  def time_in_seconds
+    @time_now - @time_start
   end
 end
