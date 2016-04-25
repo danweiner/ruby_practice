162bf92ece97c0b8058114fa180fb5148218d0ce
Dan Weiner <daweiner16@gmail.com>
2016-04-25 13:13:04 -0400

    Add disc methods

--- project_11/disc.rb
+++ project_11/disc.rb
@@ -19,13 +19,39 @@ class Disc
     @x = 0
     @y = 0
     @post = starting_post
+    # Uses code from Post class
+    starting_post.add_disc(self)
   end
 
+  # Properly place the disc on top of the post in the correct stack position
   def draw
-    @window.draw_quad(
-      @x, @y, @color,
-      @x + @width, @y, @color,
-      @x + @width, @y + @height, @color,
-      @x, @y + @height, @color)
+    if @post
+      @x = @post.center - @width / 2
+      position = @post.find_disc_positon(self)
+      if position
+        if @selected
+          c = @selected_color
+        else
+          c = @color
+        end
+        # Calculate the y position based on height of post
+        @y = @post.base - @height - (position * (@height + DISC_VERTICAL_GAP))
+        
+        @window.draw_quad(
+          @x, @y, c,
+          @x + @width, @y, c,
+          @x + @width, @y + @height, c,
+          @x, @y + @height, c)
+      end
+    end
+  end
+
+  def toggle_selected
+    @selected = !@selected
+  end
+
+  # Detect clicks inside the rectangle
+  def contains?(mouse_x, mouse_y)
+    mouse_x >= @x && mouse_x <= @x + @width && mouse_y >= @y && mouse_y <= @y + @height
   end
 end
